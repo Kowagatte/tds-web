dc54da81a2d1e8abe7ed312957081354171e8a7b
Nicholas Ryan <nnr635@usask.ca>
2022-03-03 16:08:26 -0600

    Generated react-app for client and created mysql database

--- server/server.js
+++ server/server.js
@@ -1,11 +1,28 @@
-import { WebSocketServer } from 'ws';
+const express = require('express');
+const bodyParser = require('body-parser');
+//I'm using mysql2 since I get a authorization error trying to connect to my SQL Server.
+const mysql = require('mysql2')
+const secret = require('./secret.json')
 
-const wss = new WebSocketServer({ port: 8080 });
+const PORT = 8080;
+const HOST = '127.0.0.1';
+const app = express();
 
-wss.on('connection', function connection(ws) {
-  ws.on('message', function message(data) {
-    console.log('received: %s', data);
-  });
+const con = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    database: 'tdsweb',
+    password: secret.mysql
+});
 
-  ws.send('Connection Established.');
+app.use(bodyParser.json());
+
+app.use('/', express.static('../client/build'));
+
+con.connect(function(err) {
+    if (err) console.log(err);
+    else{
+        app.listen(PORT, HOST);
+        console.log('SERVER RUNNING');
+    }
 });
