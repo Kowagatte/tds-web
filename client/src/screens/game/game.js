f29472d2504c2d1f42f9a19ae630cddb759290f2
Nicholas Ryan <nnr635@usask.ca>
2022-04-16 01:57:42 -0600

    Started messing with canvas graphics. Can draw to screen!

--- client/src/screens/game/game.js
+++ client/src/screens/game/game.js
@@ -7,32 +7,61 @@ import "./game.css"
 function GamePage() {
     const [socket, setSocket] = useState(null)
 
-    useEffect(() =>{
-      var parentSocket = getSocket();
-      if(parentSocket){
-        setSocket(parentSocket)
+
+    var playerOne = new player(140, 10, "red", 10, 120);
+
+    function startGame(){
+      game.start();
+    }
+
+    // TODO ACCEPT UPDATES FROM SERVER AND THEN DRAW THEM TO THE SCREEN.
+    // ALLOW THE PLAYER TO CONTROL A GIVEN SPRITE
+
+    var game = {
+      canvas : document.createElement("canvas"),
+      start : function(){
+        this.canvas.width = 800;
+        this.canvas.height = 600;
+        this.context = this.canvas.getContext("2d");
+        document.getElementById('gameFrame').appendChild(this.canvas);
+        this.interval = setInterval(update, 20);
+      },
+      clear : function(){
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+        this.context.globalCompositeOperation = 'destination-under';
+        // Now draw!
+        this.context.fillStyle = "white";
+        this.context.fillRect(0, 0, this.canvas.width, this.canvas.height);
       }
+    }
 
-      var c = document.getElementById("gameFrame");
-      var ctx = c.getContext("2d");
+    function player(width, height, color, x, y){
+      this.width = width;
+      this.height = height;
+      this.x = x;
+      this.y = y;
+      this.update = function(){
+        var ctx = game.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+      }
+    }
 
-      // Create gradient
-      var grd = ctx.createLinearGradient(0, 0, 800, 0);
-      grd.addColorStop(0, "blue");
-      grd.addColorStop(1, "white");
+    function update(){
+      game.clear();
+      playerOne.x += 1;
+      playerOne.update();
+    }
 
-      // Fill with gradient
-      ctx.fillStyle = grd;
-      ctx.fillRect(10, 10, 800, 600);
-
-    }, []);
+    useEffect(()=>{
+      startGame();
+    });
 
     return(
-      <center>
+      <center id={'gameFrame'}>
         <PlayerBadge/>
         <PlayerBadge/>
         <Scoreboard/>
-        <canvas id="gameFrame" width="800" height="600"></canvas>
 
       </center>
     );
